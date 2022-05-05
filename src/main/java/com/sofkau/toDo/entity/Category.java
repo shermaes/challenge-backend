15f40a066bb7cc81a1475d65d83434f8bf40d99d
Sherilyn Maestre <you@example.com>
2022-05-05 04:11:25 -0500

    created the entities

---
+++ src/main/java/com/sofkau/toDo/entity/Category.java
@@ -0,0 +1,29 @@
+package com.sofkau.toDo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "Category")
+@Table(name = "category")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private List<Task> tasks = new ArrayList<>();
+
+    public Category addTask(Task task) {
+        this.tasks.add(task);
+        return this;
+    }
+
+}
---
+++ src/main/java/com/sofkau/toDo/entity/Task.java
@@ -0,0 +1,22 @@
+package com.sofkau.toDo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity(name="Task")
+@Table(name="task")
+@Data
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String message;
+
+    private boolean done;
+
+    private Long fkTaskId;
+}
